54140e3f28ac2c7eab48fa6d086c434406ab2332
Mitchell Currey <mitchellcurrey@gmail.com>
2021-06-21 13:34:29 +1200

    Remove strict type checking on IOB recovery review threshold

--- API/v1/Dto/Input/Space/Recovery/IncreaseOverBaseRecoveryDto.cs
+++ API/v1/Dto/Input/Space/Recovery/IncreaseOverBaseRecoveryDto.cs
@@ -7,8 +7,7 @@ namespace Forbury.Integrations.API.v1.Dto.Input.Space.Recovery
     public class IncreaseOverBaseRecoveryDto : RecoveryDto
     {
         public string RecoveryCode { get; set; }
-        [JsonConverter(typeof(StringEnumConverter))]
-        public ReviewThresholdType ReviewThreshold { get; set; }
+        public string ReviewThreshold { get; set; }
         public decimal? BaseAmount { get; set; }
         public decimal? PercentageApplied { get; set; }
         public decimal? Cap { get; set; }
