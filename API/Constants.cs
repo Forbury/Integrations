878cb57f935e98f3684ad8ed61f0b87522c0fcc8
AprilHT <april.hickson@forbury.com>
2022-01-31 11:12:43 +1300

    Add base DTOs for webhooks

---
+++ API/Constants.cs
@@ -0,0 +1,7 @@
+﻿namespace Forbury.Integrations.API
+{
+    public static class Constants
+    {
+        public const string SignatureHeaderName = "Forbury-Webhook-Signature";
+    }
+}
---
+++ API/Models/Webhook/BaseEventDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Forbury.Integrations.API.Models.Webhook
+{
+    public abstract class BaseEventDto
+    {
+        public Guid UserId { get; set; }
+        public DateTimeOffset TimeStamp { get; set; }
+        public abstract WebhookType Type { get; }
+    }
+}
