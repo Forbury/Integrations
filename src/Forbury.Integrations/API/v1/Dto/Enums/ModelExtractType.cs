4dbbd7ff867bffad6160dcbb14169eaf6606d776
Mitchell Currey <mitchellcurrey@gmail.com>
2022-08-04 09:40:23 +1200

    Create Model Extraction DTOs

---
+++ src/Forbury.Integrations/API/v1/Dto/Enums/ModelExtractType.cs
@@ -0,0 +1,8 @@
+﻿namespace Forbury.Integrations.API.v1.Dto.Enums
+{
+    public enum ModelExtractionType
+    {
+        Sheet = 0,
+        Chart = 1
+    }
+}
---
+++ src/Forbury.Integrations/API/v1/Dto/File/ModelExtractionDto.cs
@@ -0,0 +1,11 @@
+﻿using Forbury.Integrations.API.v1.Dto.Enums;
+
+namespace Forbury.Integrations.API.v1.Dto.File
+{
+    public class ModelExtractionDto : FileDto
+    {
+        public ModelExtractionType ExtractionType { get; set; }
+        public string SheetName { get; set; }
+        public string Name { get; set; }
+    }
+}
