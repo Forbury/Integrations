9e978af69f34c20f99492d2e1dc68a67c8007563
Mitchell Currey <mitchellcurrey@gmail.com>
2022-08-04 14:57:04 +1200

    Move ModelExtractContentTypeExtensions

--- src/Forbury.Integrations/API/v1/Dto/Enums/ModelExtractContentType.cs
+++ src/Forbury.Integrations/API/v1/Dto/Enums/ModelExtractContentType.cs
@@ -1,24 +1,8 @@
-﻿using System;
-using System.Net.Mime;
-
-namespace Forbury.Integrations.API.v1.Dto.Enums
+﻿namespace Forbury.Integrations.API.v1.Dto.Enums
 {
     public enum ModelExtractFileType
     {
         Pdf = 0,
         Jpeg = 1
-    }
-
-    public static class ModelExtractContentTypeExtensions
-    {
-        public static string ToMediaType(this ModelExtractFileType contentType)
-        {
-            return contentType switch
-            {
-                ModelExtractFileType.Pdf => MediaTypeNames.Application.Pdf,
-                ModelExtractFileType.Jpeg => MediaTypeNames.Image.Jpeg,
-                _ => throw new NotSupportedException("Content type not supported.")
-            };
-        }
     }
 }
---
+++ src/Forbury.Integrations/API/v1/Extensions/ModelExtractContentTypeExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Forbury.Integrations.API.v1.Dto.Enums;
+using System;
+using System.Net.Mime;
+
+namespace Forbury.Integrations.API.v1.Extensions
+{
+    public static class ModelExtractContentTypeExtensions
+    {
+        public static string ToMediaType(this ModelExtractFileType contentType)
+        {
+            return contentType switch
+            {
+                ModelExtractFileType.Pdf => MediaTypeNames.Application.Pdf,
+                ModelExtractFileType.Jpeg => MediaTypeNames.Image.Jpeg,
+                _ => throw new NotSupportedException("Content type not supported.")
+            };
+        }
+    }
+}
