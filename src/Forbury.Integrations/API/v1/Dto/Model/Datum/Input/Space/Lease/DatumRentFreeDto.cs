6dd0ebec3cbf0e6f80e8708cd6c3acc64f2d7c68
Mitchell Currey <Mitchell.Currey@forbury.com>
2024-07-23 10:00:28 +0100

    Update DatumRentFreeDto.cs

--- src/Forbury.Integrations/API/v1/Dto/Model/Datum/Input/Space/Lease/DatumRentFreeDto.cs
+++ src/Forbury.Integrations/API/v1/Dto/Model/Datum/Input/Space/Lease/DatumRentFreeDto.cs
@@ -7,7 +7,6 @@ namespace Forbury.Integrations.API.v1.Dto.Model.Datum.Input.Space.Lease
     public class DatumRentFreeDto
     {
         [JsonConverter(typeof(DateFormatConverter), JsonFormats.DateFormat)] public DateTime? Date { get; set; }
-
         public decimal Months { get; set; }
         public decimal Percent { get; set; }
     }
