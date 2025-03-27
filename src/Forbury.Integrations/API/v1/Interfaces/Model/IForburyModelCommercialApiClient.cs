4476eb5d4f6c1f4b0eb95ab82457ae00ceb6cebc
Seyoun Lim <imseyoun@gmail.com>
2025-03-27 14:30:57 +1300

    Support 'Commercial' creation to client API

--- src/Forbury.Integrations/API/v1/Interfaces/Model/IForburyModelCommercialApiClient.cs
+++ src/Forbury.Integrations/API/v1/Interfaces/Model/IForburyModelCommercialApiClient.cs
@@ -1,12 +1,21 @@
 ﻿using System.Threading;
 using System.Threading.Tasks;
+using Forbury.Integrations.API.v1.Dto.Model;
 using Forbury.Integrations.API.v1.Dto.Model.Commercial;
+using Forbury.Integrations.API.v1.Dto.Model.Commercial.Input;
 
 namespace Forbury.Integrations.API.v1.Interfaces.Model
 {
     public interface IForburyModelCommercialApiClient : IForburyModelApiClient
     {
         Task<ModelCommercialDetailedDto> GetModelById(int modelId,
+            CancellationToken cancellationToken = default);
+
+        Task<ModelDto> CreateModel(ValuationInputDto data,
+            string googlePropertyId,
+            int? teamId,
+            string externalId = null,
+            string fullAddress = null,
             CancellationToken cancellationToken = default);
     }
 }
