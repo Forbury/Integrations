f052976353f13218b817b840e6d47ebc046cf81f
Johan <nzjohan187@gmail.com>
2025-09-24 14:13:36 +1200

    Cleaning up

--- src/Forbury.Integrations/API/v1/Dto/Model/Commercial/Input/Outgoings/OutgoingsRecoveryDto.cs
+++ src/Forbury.Integrations/API/v1/Dto/Model/Commercial/Input/Outgoings/OutgoingsRecoveryDto.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Forbury.Integrations.API.v1.Dto.Model.Commercial.Input.Outgoings
+﻿namespace Forbury.Integrations.API.v1.Dto.Model.Commercial.Input.Outgoings
 {
     public class OutgoingsRecoveryDto
     {
